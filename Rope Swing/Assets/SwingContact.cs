53b239b1462e6625c263cede31c97403cd43bf70
CJAssassin284 <42413778+CJAssassin284@users.noreply.github.com>
2019-01-21 16:04:56 -0500

    Current Version

---
+++ Rope Swing/Assets/SwingContact.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingContact : MonoBehaviour
+{
+    public bool canGrab = false;
+    public Transform grabObj;
+    public Vector3 offset;
+
+
+    bool grabbed;
+    private void Update()
+    {
+        //If touch when near rope allow to grab.
+        if(canGrab)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                grabbed = true;
+            }
+        }
+
+        //If the player is on the rope
+        if(grabbed)
+        {
+                transform.position = grabObj.transform.position + offset;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Detects if we are colliding with the rope.
+        if(collision.gameObject.CompareTag("Rope"))
+        {
+            canGrab = true;
+            grabObj = collision.transform;
+        }
+    }
+
+}
