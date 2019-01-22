d3b95cf8b2221c8d92a4290ad0c3b708dd6dc999
Zeeshan Mohammed <zee_shan265@hotmail.com>
2019-01-21 21:30:43 -0500

    game over screen, ui for coins and distance score

---
+++ Rope Swing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+    [Header("Player Properties")]
+    public GameObject player;
+    public int coins, highScore;
+
+    [Header("HUD Properties")]
+    public GameObject coinsData;
+    public GameObject highScoreData;
+    public TextMeshProUGUI coinsText, highScoreText;
+
+    [Header("Game Over UI Properties")]
+    public GameObject gameOverUI;
+    public TextMeshProUGUI totalCoinsText, totalScoreText;
+    public Button mainMenuButton, retryButton;
+
+    bool isDead = false;
+
+    private void Awake() {
+        //coins = 0;
+        //highScore = 0;
+        DisplayHUD();
+    }
+
+    private void Update() {
+        if (player.transform.position.y <= -2.5f) { 
+            isDead = true;
+        }
+        Debug.Log("Is Dead: " + isDead);
+
+        if (isDead) {
+            GameOver();
+        }
+    }
+
+    void DisplayHUD() {
+        coinsText.text = coins.ToString();
+        highScoreText.text = highScore.ToString() + "m";
+        gameOverUI.SetActive(false);
+    }
+
+    void HideHUD() {
+        coinsData.SetActive(false);
+        highScoreData.SetActive(false);
+    }
+
+    public void MainMenu() {
+
+    }
+
+    public void Retry() {
+
+    }
+
+    void GameOver() {
+        HideHUD();
+        gameOverUI.SetActive(true);
+        totalCoinsText.text = coins.ToString();
+        totalScoreText.text = highScore.ToString() + "m";
+    }
+
+}
